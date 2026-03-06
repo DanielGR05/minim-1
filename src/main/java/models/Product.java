f2dbeb3e7650ee82816d173543bc8f400f08a2d6
PauGil <pau.gil.gomez@estudiantat.upc.edu>
2026-03-06 15:54:15 +0100

    ÃProduct advanced v1

--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -5,18 +5,32 @@ public class Product {
     private  String id;
     private  String name;
     private double price;
+    private int sales;
 
     public Product(String id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.sales = 0;
     }
 
-    public int sales() {
-        return 0;
+    public String getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
     public double getPrice() {
         return price;
+    }
+
+    public int sales() {
+        return this.sales;
+    }
+
+    public void addSales(int quantity){
+        this.sales += quantity;
     }
 }
