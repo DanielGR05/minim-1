d5b4ea816232a2244e6de4299b31892f2a6c3600
PauGil <pau.gil.gomez@estudiantat.upc.edu>
2026-03-12 19:01:10 +0100

    First version of Order

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,18 +1,29 @@
 package models;
 
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Order {
-    // private String dni;
-    private Queue<Product> comanda;
+    private String dni;
+    private List<String> comanda;
 
     public Order(String dni) {
+        this.dni = dni;
+        this.comanda = new ArrayList<String>();
     }
 
     public void addLP(int i, String s) {
+        for(int j=0; j<i; i++ ){
+            this.comanda.add(s);
+        }
     }
 
     public String getUser() {
-        return null;
+        return this.dni;
+    }
+
+    public List<String> getComanda() {
+        return this.comanda;
     }
 }
